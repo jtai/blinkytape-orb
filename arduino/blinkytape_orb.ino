1d57ca76a16440bfa1278fbb7889c53a4bd89d61
Jon Tai <jon.tai@gmail.com>
2016-01-23 20:52:43 -0800

    Refactor fade loops into a common method

--- arduino/blinkytape_orb.ino
+++ arduino/blinkytape_orb.ino
@@ -1,73 +1,69 @@
 #include "FastLED.h"
 
-/* hardware */
+// hardware
 #define NUM_LEDS 12
 
 #define DATA_PIN 3
 
-/* visuals */
+// visuals
 #define PULSE_MAX_VAL 255
 #define PULSE_MIN_VAL 160
-#define PULSE_CYCLE 2000
-#define PULSE_DELAY PULSE_CYCLE / (PULSE_MAX_VAL - PULSE_MIN_VAL)
+#define PULSE_DURATION 3000
 
 #define CHANGE_MIN_VAL 64
-#define CHANGE_CYCLE 500
-#define CHANGE_DELAY CHANGE_CYCLE / (PULSE_MIN_VAL - CHANGE_MIN_VAL)
+#define CHANGE_DURATION 1000
 
-/* demo mode */
+// demo mode
 #define DEMO_PULSES 5
 
-/* state */
+// state
 CRGB leds[NUM_LEDS];
 uint8_t hue = HUE_RED;
+uint8_t val = PULSE_MAX_VAL;
 uint8_t demo_pulse_count = 0;
+
+
+
+void fade(uint8_t new_val, uint16_t duration) {
+  uint8_t incr;
+  uint8_t delay_ms;
+  if (val < new_val) {
+    incr = 1;
+    delay_ms = duration / (new_val - val);
+  } else if (val > new_val) {
+    incr = -1;
+    delay_ms = duration / (val - new_val);
+  } else {
+    delay(duration);
+    return;
+  }
+
+  for (uint8_t v = val; v != new_val; v += incr) {
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i] = CHSV(hue, 255, v);
+    }
+    FastLED.show();
+    delay(delay_ms);
+  }
+
+  val = new_val;
+}
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
   for (uint8_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hue, 255, PULSE_MAX_VAL);
+    leds[i] = CHSV(hue, 255, val);
   }
 }
 
 void loop() {
   demo_pulse_count = (demo_pulse_count + 1) % DEMO_PULSES;
-
-  for (uint8_t v = PULSE_MAX_VAL; v > PULSE_MIN_VAL; v--) {
-    for (uint8_t i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(hue, 255, v);
-    }
-    FastLED.show();
-    delay(PULSE_DELAY);
-  }
-
-  /* demo mode */
+  fade(PULSE_MIN_VAL, PULSE_DURATION / 2);
   if (demo_pulse_count == 0) {
-    for (uint8_t v = PULSE_MIN_VAL; v > CHANGE_MIN_VAL; v--) {
-      for (uint8_t i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(hue, 255, v);
-      }
-      FastLED.show();
-      delay(CHANGE_DELAY);
-    }
-
+    fade(CHANGE_MIN_VAL, CHANGE_DURATION / 2);
     hue = (hue + 32) % 255;
-
-    for (uint8_t v = CHANGE_MIN_VAL; v < PULSE_MIN_VAL; v++) {
-      for (uint8_t i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(hue, 255, v);
-      }
-      FastLED.show();
-      delay(CHANGE_DELAY);
-    }
+    fade(PULSE_MIN_VAL, CHANGE_DURATION / 2);
   }
-
-  for (uint8_t v = PULSE_MIN_VAL; v < PULSE_MAX_VAL; v++) {
-    for (uint8_t i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(hue, 255, v);
-    }
-    FastLED.show();
-    delay(PULSE_DELAY);
-  }
+  fade(PULSE_MAX_VAL, PULSE_DURATION / 2);
 }
